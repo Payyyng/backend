48fef63f0a6d984d34c4e590ab6dfc42ae45b36d
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2023-05-07 18:29:10 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   // await app.listen(3000);
-  await app.listen(3000 || process.env.PORT);
+  await app.listen(process.env?.PORT || 3000 );
 
 }
 bootstrap();
