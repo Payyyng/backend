2f0ca90de513bdc57343d7380a5fa48c72c55306
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2023-05-26 06:46:28 -0500

    update

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CardService } from './card.service';
 import { CardController } from './card.controller';
-import { MailService } from 'src/mail/mail.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { MailService } from '../mail/mail.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  providers: [CardService, MailService],
-  controllers: [CardController]
+  providers: [CardService],
+  controllers: [CardController],
+  imports: [PrismaModule,MailModule]
 })
 export class CardModule {}
