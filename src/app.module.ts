9040a1c70d4a854482b3b13578fb81b3421ddef9
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2023-05-26 10:10:39 -0500

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,8 +16,8 @@ import { AdminController } from './admin/admin.controller';
 import { AdminService } from './admin/admin.service';
 import { ConfigModule } from '@nestjs/config';
 import { CardModule } from './card/card.module';
-import { APP_FILTER } from '@nestjs/core';
-import { HttpExceptionFilter } from './http-exception.filter';
+// import { APP_FILTER } from '@nestjs/core';
+// import { HttpExceptionFilter } from './http-exception.filter';
 
 
 @Module({
@@ -44,10 +44,10 @@ import { HttpExceptionFilter } from './http-exception.filter';
     UsersService,
     TransactionService,
     AdminService,
-    {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
-    },
+    // {
+    //   provide: APP_FILTER,
+    //   useClass: HttpExceptionFilter,
+    // },
   ],
 
 
