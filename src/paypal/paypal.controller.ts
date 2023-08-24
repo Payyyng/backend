fee841d83c7277cf0a1fde38981a03dfaa3fcbbe
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2023-08-24 13:24:11 -0500

    update

--- src/paypal/paypal.controller.ts
+++ src/paypal/paypal.controller.ts
@@ -5,51 +5,63 @@ import { UpdatePaypalDto } from './dto/update-paypal.dto';
 import { MakePayment } from './dto/make-a-payment.dto';
 import { BuyPaypalDTO } from './dto/buy-paypal-dto';
 import { SellPaypalDTO } from './dto/sell-paypal-dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Controller('paypal')
 export class PaypalController {
   constructor(private readonly paypalService: PaypalService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createPaypalDto: CreatePaypalDto) {
     return this.paypalService.createPaypal(createPaypalDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.paypalService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.paypalService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePaypalDto: UpdatePaypalDto) {
     return this.paypalService.update(+id, updatePaypalDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.paypalService.remove(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post("make-a-payment")
   MakeAPayment(@Body() makeAPayment: MakePayment) {
     return this.paypalService.makeApayment(makeAPayment);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post("buy-paypal")
   BuyPaypal(@Body() buyPaypal: BuyPaypalDTO) {
     return this.paypalService.buyPaypal(buyPaypal);
   }
 
+
+  @UseGuards(JwtAuthGuard)
   @Post("sell-paypal")
   SellPaypal(@Body() sellPaypal: SellPaypalDTO) {
     return this.paypalService.sellPaypal(sellPaypal);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post("sell-wise")
   wiseDeposit(@Body() deposit: MakePayment) {
     return this.paypalService.wiseDeposit(deposit);
