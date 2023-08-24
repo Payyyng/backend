8fdf119a1420ccd29e50ed2f2ec3cfe17bf2f665
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2023-08-24 12:46:20 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('payyng-api', app, document);
 
   // const port = process.env.PORT || 3000;
 
