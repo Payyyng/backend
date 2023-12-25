2c16ae3071f6ac6ed99c2bc3067b7a7a7d2047b5
Boyinbode Ebenezer Ayomide <boyinbodedev@gmail.com>
2023-12-25 09:42:31 +0100

    updates

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -112,7 +112,7 @@ export class UsersService {
     }
 
     const bvn = process.env.ADMIN_BVN
-    await this.createBankAccount(email, bvn)
+    await this.createBankAccount(newUser.email, bvn)
     await this.mailService.sendVerificationMail(email, firstName, otp);
 
     return {
